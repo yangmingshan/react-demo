5aa6e953e7f16ad9e85629c513de2ee6e3b1b32b
杨明山 <y.mingshan3@gmail.com>
2019-08-01 16:23:57 +0800

    Update stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   extends: [
     'stylelint-config-standard',
+    'stylelint-config-css-modules',
     isProd ? 'stylelint-prettier/recommended' : 'stylelint-config-prettier'
   ]
 };
